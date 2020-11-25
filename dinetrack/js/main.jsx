36315eb5d2a6319ffc39b01362260ebe89208a10
ekaraasl <ekaraasl@umich.edu>
2020-11-24 16:24:19 -0800

    Revert "Tip tool works for a single input need to add functionality for react to update multiple tippers" Pushed to the wrong branch going back to correct prototype brancg commit This reverts commit 559e531562fc23d7122d1e7ffcdbdb077353305c.

--- dinetrack/js/main.jsx
+++ dinetrack/js/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './index';
-import TipIndex from './tip';
 
 // This method is only called once
 ReactDOM.render(
   <div>
-  	<TipIndex/>
+  	<Index/>
   </div>,
   document.getElementById('reactEntry'),
 );
