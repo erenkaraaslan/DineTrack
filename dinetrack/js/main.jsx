559e531562fc23d7122d1e7ffcdbdb077353305c
ekaraasl <ekaraasl@umich.edu>
2020-11-24 16:19:00 -0800

    Tip tool works for a single input need to add functionality for react to update multiple tippers

--- dinetrack/js/main.jsx
+++ dinetrack/js/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './index';
+import TipIndex from './tip';
 
 // This method is only called once
 ReactDOM.render(
   <div>
-  	<Index/>
+  	<TipIndex/>
   </div>,
   document.getElementById('reactEntry'),
 );
--- dinetrack/js/tip.jsx
+++ dinetrack/js/tip.jsx
@@ -1,56 +1,139 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+let uniqueIndex = 0;
+
+function updateState(percent) {
+  this.setState({
+      tipPercentage: percent,
+      tipToPay: this.state.inputAmount * percent / 100.0,
+  });
+}
+
+function addUnique(index) {
+  this.setState({
+      id: index,
+  });
+  uniqueIndex++;
+}
+
 class Tip extends React.Component {
 
   constructor(props) {
     // Initialize mutable state
     super(props);
     this.state = {
+        id: '',
+        name: '',
         inputAmount: '',
         tipPercentage: '',
         tipToPay: '',
     };
+    updateState = updateState.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.updateAmount = this.updateAmount.bind(this);
+    //this.handleTip = this.handleTip.bind(this);
   }
 
   componentDidMount() {
+    this.setState({
+      tipPercentage: '20',
+    });
   }
 
-  handleClick(event) {
-    const value = event.target.value; 
-    switch(value) {
-        case 'submit': {
-            var currTipToPay = '';
-            if (this.inputAmount !== '' && this.tipPercentage !== '') {
-                currTipToPay = calculateTip(this.inputAmount, this.tipPercentage);
-                if (currTipToPay===undefined) 
-                    this.setState({tipToPay: "Math Error"}); 
-                else
-                    this.setState({ tipToPay: currTipToPay}); 
-                break; 
-            }
-        }
-    }
+  updateName(event) {
+    this.setState({
+      name: event.target.value,
+    });
   }
-  
-  calculateTip(_inputAmount, _tipPercentage) {
-    return _inputAmount * _tipPercentage / 100;
+
+  updateAmount(event) {
+    this.setState({
+      inputAmount: event.target.value,
+    });
   }
+
+  /*handleTip(event) {
+    event.preventDefault();
+    console.log("In handleTip");
+    this.setState({
+      tipToPay: this.state.inputAmount * this.state.tipPercentage / 100.0,
+    });
+  }*/
 
   render() {
-
+    let output;
+    if(this.state.tipToPay) {
+      output = <p>{this.state.name} must tip {this.state.tipToPay} at {this.state.tipPercentage} percent</p>;
+    }
     return (
-      <div>
-        <p>
-          Hello World of React!
-        </p>
+      <div className="tip">
+        <input type="text" placeholder="Name" value={this.state.name} onChange={this.updateName}/>
+        <input type="text" placeholder="Amount" value={this.state.inputAmount} onChange={this.updateAmount}/>
+        {output}
       </div>
     );
   }
 }
 
-Tip.propTypes = {
-  url: PropTypes.string.isRequired,
-};
+class TipIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {numTippers: '', slideValue: '', tipList: []};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleParty = this.handleParty.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-export default Tip;
+  componentDidMount() {
+    this.setState({
+      slideValue: 20,
+      numTippers: 0,
+    });
+  }
+
+  handleParty(event) {
+    event.preventDefault();
+    this.setState({
+      numTippers: event.target.value,
+    });
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      slideValue: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.numTippers);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    updateState(this.state.slideValue);
+  }
+
+  render() {
+    const tip = <Tip/>;
+    return (
+      <div>
+        <p>How many people will be paying for this meal?</p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Party Size" value={this.state.numTippers} onChange={this.handleParty}/>
+        </form>
+        {tip}
+        <div>
+          <p>{this.state.slideValue}%</p>
+          <input type="range" min="0" max="100" step="1" value={this.state.slideValue} onChange={this.handleChange}/>
+          <button onClick={this.handleClick}>Calculate Tip!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TipIndex;
